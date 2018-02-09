ffb8c355422a634f5cc58853fd9a8b4380db1a21
yevhen-m <yvhn.yvhn@gmail.com>
2018-02-09 19:35:52 +0200

    Add checker

---
+++ flake8_fancy_header/checker.py
@@ -0,0 +1,48 @@
+__version__ = '0.1.0'
+
+
+import ast
+import os.path
+
+
+class FancyHeaderChecker(object):
+    name = 'flake8-fancy-header'
+    version = __version__
+
+    message_missing = 'S011 Fancy header. Missing.'
+    message_invalid = 'S012 Fancy header. Invalid.'
+
+    def __init__(self, tree, filename):
+        self.tree = tree
+        self.filename = filename
+
+    def get_header_value(self):
+        import_path = os.path.splitext(self.filename)[0].replace('/', '.')
+        border = '=' * len(import_path)
+        return '\n'.join(('', border, import_path, border, ''))
+
+
+    def run(self):
+        body = self.tree.body
+        if not body:
+            yield (
+                1, 1, self.message_missing, type(self),
+            )
+            return
+
+        if not isinstance(body[0], ast.Expr):
+            yield (
+                1, 1, self.message_missing, type(self),
+            )
+            return
+
+        if not isinstance(body[0].value, ast.Str):
+            yield (
+                1, 1, self.message_missing, type(self),
+            )
+            return
+
+        if not body[0].value.s == self.get_header_value():
+            yield (
+                1, 1, self.message_invalid, type(self),
+            )
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+install_requires = [
+    "flake8 > 3.0.0",
+]
+
+setuptools.setup(
+    name="flake8_fancy_header",
+    license="MIT",
+    version="0.1.0",
+    description="check file has a fancy header at the top",
+    author="yevhen-m",
+    url="https://github.com/yevhen-m/flake8-fancy-header",
+    packages=[
+        "flake8_fancy_header",
+    ],
+    test_suite='tests',
+    install_requires=install_requires,
+    entry_points={
+        'flake8.extension': [
+            'S01 = flake8_fancy_header:FancyHeaderChecker',
+        ],
+    },
+    classifiers=[
+        "Framework :: Flake8",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance",
+    ],
+)
